459764b86923118039ce6de047f8a851a8057c8d
praneedangeti <praneedangeti@gmail.com>
2025-12-03 20:10:18 +0530

    Add files via upload

---
+++ Program to find area of circle in c.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int R;
+	float area;
+	printf("enter the radius:");
+	scanf("%d",&R);
+	area = 3.14 *R* R;
+	printf("areaof circle is %f",area);
+	return 0;
+	
+	
+}
