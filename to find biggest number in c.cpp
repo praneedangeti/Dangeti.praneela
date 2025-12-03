e5d0404726de824bcac47f741c858fc9d016bf60
praneedangeti <praneedangeti@gmail.com>
2025-12-03 19:27:59 +0530

    Add files via upload

---
+++ to find biggest number in c.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int  main()
+ {
+ 	int age;
+printf("enter your age :") ;
+scanf("%d",age);
+if(age>=18)
+printf("eligible for vote");
+else
+printf("Not eligible for vote");
+return 0;
+}
+
