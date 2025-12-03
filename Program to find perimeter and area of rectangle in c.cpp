d4b3fd38f770bd7bcf56f5fa25f51acd980fa018
praneedangeti <praneedangeti@gmail.com>
2025-12-03 20:36:14 +0530

    Add files via upload

---
+++ Program to find perimeter and area of rectangle in c.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int  perimeter,length,width,area;
+printf("enter length:");
+scanf("%d",&length);
+printf("enter width:");
+scanf("%d,&breath");
+perimeter = 2*(length + width);
+area = length*width;
+return 0;
+}
